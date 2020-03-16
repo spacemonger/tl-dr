17e374fa2ccb6b7a9b1ebe260ae79f78c89a8d81
spacemonger <frankli.life@gmail.com>
2020-03-16 11:12:33 -0400

    deleted a function

--- content.js
+++ content.js
@@ -1,14 +1,12 @@
 var searches = document.getElementsByClassName("B6fmyf");
 var links = document.getElementsByClassName("r").getElementsById("a").href;
-var url =
+
+
 var i;
 for (i = 0; i < searches.length; i++) {
-  searches[i].setAttribute("data-url", links[i]);
-}
-
-function addElement () {
 
   var newDiv = document.createElement("div");
-
-  
+  newDiv.innerText = "TL;DR" ;
+  newDiv[i].setAttribute("data-url", links[i]);
+  searches[i].appendChild(newDiv);
 }
