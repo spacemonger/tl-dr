f1405872c83d0876a398ee836a7cc118bc57760b
spacemonger <fli1@ocdsb.ca>
2020-03-15 22:06:08 -0400

    working to add tl;dr div into DOM

---
+++ content.js
@@ -0,0 +1,14 @@
+var searches = document.getElementsByClassName("B6fmyf");
+var links = document.getElementsByClassName("r").getElementsById("a").href;
+var url = 
+var i;
+for (i = 0; i < searches.length; i++) {
+  searches[i].setAttribute("data-url", links[i]);
+}
+
+function addElement () {
+
+  var newDiv = document.createElement("div");
+
+  var
+}
