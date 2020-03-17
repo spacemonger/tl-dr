cb1083fb08d3ece3c59787c0bd2e9212b35260bc
spacemonger <frankli.life@gmail.com>
2020-03-16 22:20:22 -0400

    Message Passing between contentScript & background

--- content.js
+++ content.js
@@ -2,24 +2,32 @@ window.onload = function () {
   //create HTMLCollection of all B6fmyf divs
   var searches = document.querySelectorAll(".B6fmyf");
   var links = [];
+  
+  
 
 var i;
 for (i = 0; i < searches.length; i++) {
 
+  
   links[i] = searches[i].previousSibling.href;
   
-  var newDiv = document.createElement("div");
-  newDiv.innerText = "TL;DR" ;
-  newDiv.className = ("tl-dr-sum-button");
-  newDiv.setAttribute("data-url", links[i]);
-  console.log(links[i]);
+  (function() {
+    var newDiv = document.createElement("div");
+    newDiv.innerText = "TL;DR" ;
+    newDiv.className = ("tl-dr-sum-button");
+    newDiv.setAttribute("data-url", links[i]);
+    newDiv.addEventListener("click", function(){
+      chrome.runtime.sendMessage(newDiv.dataset.url, (response) => {
+        console.log(response);
+      });
+      });
+      
+    searches[i].appendChild(newDiv); //add tl;dr on the side
+  }());
 
-  searches[i].appendChild(newDiv); //add tl;dr on the side
-  
-  searches[i].querySelector(".tl-dr-sum-button").addEventListener("click", function(){
-    console.log(links[i]);
-  });
-  }
+
+ 
+}
 }
 
 
