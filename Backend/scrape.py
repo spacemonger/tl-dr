194a108c4be2e10ab0ccee78f1700c075e71cc92
spacemonger <frankli.life@gmail.com>
2020-03-26 18:16:21 -0400

    Resolved merge conflict

--- Backend/scrape.py
+++ Backend/scrape.py
@@ -2,34 +2,3 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 
-page = requests.get('https://en.wikipedia.org/wiki/Taylor_series')
-
-soup = BeautifulSoup(page.text, 'lxml')
-
-'''
-for img in soup.find_all('img'):
-    img.decompose()
-for a in soup.find_all('a'):
-    a.unwrap()
-for comments in soup.find_all('comment'):
-     comments.decompose()
-for ul in soup.find_all('ul'):
-    ul.unwrap()
-for li in soup.find_all('li'):
-    li.unwrap()
-for i in soup.find_all('i'):
-    i.unwrap()
-for span in soup.find_all('span'):
-    span.unwrap()
-for cite in soup.find_all('cite'):
-    cite.decompose()
-for link in soup.find_all('link'):
-    link.decompose
-soup.smooth()
-
-
-print(soup.prettify())
-'''
-
-
-print(soup.find("div", id = "content"))
