6f7a03d6800ff353907e6bc75136564a2251c7f0
urugly12 <62062723+urugly12@users.noreply.github.com>
2020-04-19 23:07:03 -0400

    image doesn't display

--- FrontEnd/content.js
+++ FrontEnd/content.js
@@ -46,13 +46,80 @@ window.onload = function () {
 </div>
   `
   
- 
+ var templateLoading = `
+ <head>
+    <style>
+
+    #tl-dr-wrapper {
+        visibility: visible;
+        background-color: #F6F6F6; 
+        width: 420px; 
+        height: 210px; 
+        overflow-x: hidden; 
+        overflow-y: auto;
+        text-align: center; 
+        padding: 20px;
+        right: 20px;
+        top: 20px;
+        position: fixed;
+        font-size: 16px;
+        z-index: 99999999; /*to order to the front should look into*/
+        border: 0px;
+        border-radius: 10px;
+        margin-left: auto;
+        margin-right: auto;
+        max-height: calc(100vh - 40px);
+        box-shadow: 0 2px 5px 0 rgba(0,0,0,0.49);
+        color: rgb(30,30,30);
+    }
+
+    @font-face {
+        font-family: 'questrial';
+        src: url(/fonts/Questrial-Regular.ttf);
+        font-style: normal;
+        font-weight: normal;
+    }
+
+    @keyframes loading {
+        /* 0% {
+            transform: rotate (120deg); 
+        } 25% {
+            transform: rotate (200deg);
+        } 50% {
+            transform: rotate (270deg);
+        } 75% {
+            transform: rotate (315deg);
+        } 100% {
+            transform: rotate (360deg);
+        } */
+        from {
+            transform: rotate(0deg);
+        } to {
+            transform: rotate(359deg);
+        }
+    }
+
+    h1 {
+      font-size: 27px;
+      font-family: questrial;
+      font-style: bold;
+    }
+
+    h2 {
+        animation: loading 2s infinite linear;
+    }
+    </style>
+</head>
+<body>
+    <div id= "tl-dr-content">
+        <h1>Just a Moment...</h1>
+        <h2> <img src="/images/loading-yellow.png"/>
+    </div>
+</body> `
   
 
 var i;
 for (i = 0; i < searches.length; i++) {
-
-  
   links[i] = searches[i].previousSibling.href;
   
   (function() {
@@ -76,22 +143,25 @@ for (i = 0; i < searches.length; i++) {
 
 }
 
+var opened=-1;
 document.addEventListener("click", function(event){
   if(event.target.closest('#tl-dr-popup')) return;
   if(event.target.closest('.tl-dr-sum-button')) {
-    render(template, document.querySelector('#tl-dr-popup').shadowRoot.querySelector('#tl-dr-wrapper'));
-    document.querySelector('#tl-dr-popup').shadowRoot.querySelector('#tl-dr-wrapper').style.visibility = "visible";
+    if (opened==-1) {
+      render(templateLoading, document.querySelector('#tl-dr-popup').shadowRoot.querySelector('#tl-dr-wrapper'));
+      document.querySelector('#tl-dr-popup').shadowRoot.querySelector('#tl-dr-wrapper').style.visibility = "visible";
+      opened=0;
+    } else {
+      document.querySelector('#tl-dr-popup').shadowRoot.querySelector('#tl-dr-wrapper').style.visibility = "hidden";
+      opened=-1;
+    }
     return;
   }
-
   document.querySelector('#tl-dr-popup').shadowRoot.querySelector('#tl-dr-wrapper').style.visibility = "hidden";
-
 });
 
-var render = function(template, node) {
+var render = function(templateLoading, node) {
     if (!node) return;
-    node.innerHTML = template;
+    node.innerHTML = templateLoading;
 };
 }
-
-
